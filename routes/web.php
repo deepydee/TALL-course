2db8c501896d65b97d86552ed38540a1243b4002
Vitaliy Semenov <synthex@bk.ru>
2022-08-08 18:07:19 +0300

    Prepare the Subscribers System to Have a Dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,4 @@ Route::get('/', function () {
 Route::get('/subscribers/verify/{subscriber}', [SubscriberController::class, 'verify'])
 ->middleware('signed')->name('subscibers.verify');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
